c7b9a859c26dc87ab7eacad3de2ed77eb0c83f3e
tomohiro <to.tomo.tomohiro@gmail.com>
2020-01-04 12:08:45 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-root 'user#index'
+root 'users#index'
+
+get  'user'               => 'users#show'
+get  'notice'             => 'users#notice'
 
 end

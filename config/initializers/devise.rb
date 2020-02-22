1e576625fb09157f07d8f599dd26e77d3a730864
tomohiro <to.tomo.tomohiro@gmail.com>
2020-02-22 13:37:33 +0900

    ツイッターログインcallbackアドレスの修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -300,7 +300,7 @@ Devise.setup do |config|
 
   # 以下はSNSアカウントによるログイン機能の実装について記事を参照したときのもの
   # config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email', callback_url: "#{ENV['HOST']}/users/auth/facebook/callback"
-  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], scope: 'email', oauth_callback: "#{ENV['HOST']}/users/auth/twitter/callback"
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], scope: 'email', oauth_callback: "https://dokusyo-no-wa.com/users/auth/twitter/callback"
   # config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'email', redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
   # OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 end

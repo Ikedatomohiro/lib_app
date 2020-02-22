ffcbabf1ec7830577c14091f52d1ae2ec949a86b
tomohiro <to.tomo.tomohiro@gmail.com>
2020-02-22 21:30:07 +0900

    ツイッターログインのエラー対処

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,7 +29,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       session[:user_id] = @user.id #追加されていたので追加
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except("extra")
       redirect_to new_user_registration_url
     end
   end

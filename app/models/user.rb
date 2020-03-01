326eda9e10956ed1d5f9ecd6340e8585e62e4371
tomohiro <to.tomo.tomohiro@gmail.com>
2020-03-01 11:32:43 +0900

    感想ページの表示やファイル選択ボタンの修正。ボタンの動作は実装途中。

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,28 +11,28 @@ class User < ApplicationRecord
     user = User.where(uid: auth.uid, provider: auth.provider).first
     if user
       user.update(
-        image:    auth.info.image,
-        name:     auth.info.name,
-        nickname: auth.info.nickname,
-        location: auth.info.location,
-        oauth_token: auth.credentials.token,
+        image:              auth.info.image,
+        name:               auth.info.name,
+        nickname:           auth.info.nickname,
+        location:           auth.info.location,
+        oauth_token:        auth.credentials.token,
         oauth_token_secret: auth.credentials.secret,
-        twitter_link: auth.info.urls.Twitter,
+        twitter_link:       auth.info.urls.Twitter,
         )
 
     else
       user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    User.dummy_email(auth),
-        password: Devise.friendly_token[0, 20],
-        image:    auth.info.image,
-        name:     auth.info.name,
-        nickname: auth.info.nickname,
-        location: auth.info.location,
-        oauth_token: auth.credentials.token,
+        uid:                auth.uid,
+        provider:           auth.provider,
+        email:              User.dummy_email(auth),
+        password:           Devise.friendly_token[0, 20],
+        image:              auth.info.image,
+        name:               auth.info.name,
+        nickname:           auth.info.nickname,
+        location:           auth.info.location,
+        oauth_token:        auth.credentials.token,
         oauth_token_secret: auth.credentials.secret,
-        twitter_link: auth.info.urls.Twitter,
+        twitter_link:       auth.info.urls.Twitter,
       )
     end
  

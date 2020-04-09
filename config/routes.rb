ae699a051bc410f665c1ce2de3964f287a12da86
tomohiro <to.tomo.tomohiro@gmail.com>
2020-04-09 19:51:14 +0900

    本棚のレイアウトを変更するボタンを実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,37 +6,38 @@ Rails.application.routes.draw do
 
 root 'users#index'
 
-get  'notice'                           => 'users#notice'
-post 'notice'                           => 'users#notice'
-get  'shelf'                            => 'users#shelf'
-get  'setting'                          => 'users#setting'
-get  'users/update_setting'             => 'users#update_setting'
-post 'users/update_self_introduction'   => 'users#update_self_introduction' 
-post 'users/publish_impression'         => 'users#update_publish_impression'
-get  'readinghistory'                   => 'users#reading_history'
-get  'request'                          => 'users#request_page'
-get  'users/terms_of_service'           => 'users#terms_of_service'
-get  'users/privacy_policy'             => 'users#privacy_policy'
-post 'users/send_request'               => 'users#send_request'
-get  'users/release_note'               => 'users#release_note'
+get  'notice'                              => 'users#notice'
+post 'notice'                              => 'users#notice'
+get  'shelf'                               => 'users#shelf'
+get  'setting'                             => 'users#setting'
+get  'users/update_setting'                => 'users#update_setting'
+post 'users/update_self_introduction'      => 'users#update_self_introduction' 
+post 'users/publish_impression'            => 'users#update_publish_impression'
+get  'readinghistory'                      => 'users#reading_history'
+get  'request'                             => 'users#request_page'
+get  'users/terms_of_service'              => 'users#terms_of_service'
+get  'users/privacy_policy'                => 'users#privacy_policy'
+post 'users/send_request'                  => 'users#send_request'
+get  'users/release_note'                  => 'users#release_note'
+get  'users/change_shelf_type/:shelf_type' => 'users#change_shelf_type'
 resources :users
 
-get  'books/search_books_result'        => 'books#search_books_result'
-post 'books/search'                     => 'books#search_books'
-get  'books/search'                     => 'books#search_books'
-get  'books/show_form'                  => 'books#show_search_form'
-get  'books/info/:api_id'               => 'books#show_book_info'
-post 'books/set_reading_date'           => 'books#set_reading_date'
-patch 'books/show_reading_date'         => 'books#show_reading_date'
-patch 'books/update_thumbnail/:book_id'  => 'books#update_thumbnail'
+get  'books/search_books_result'           => 'books#search_books_result'
+post 'books/search'                        => 'books#search_books'
+get  'books/search'                        => 'books#search_books'
+get  'books/show_form'                     => 'books#show_search_form'
+get  'books/info/:api_id'                  => 'books#show_book_info'
+post 'books/set_reading_date'              => 'books#set_reading_date'
+patch 'books/show_reading_date'            => 'books#show_reading_date'
+patch 'books/update_thumbnail/:book_id'    => 'books#update_thumbnail'
 resources :books do
     put :sort
 end
 
-post 'impressions/add_impression_field' => 'impressions#add_impression_field'
-post 'impressions/post_to_twitter'      => 'impressions#post_to_twitter'
+post 'impressions/add_impression_field'    => 'impressions#add_impression_field'
+post 'impressions/post_to_twitter'         => 'impressions#post_to_twitter'
 resources :impressions
 
-get  'contacts/thanks'                  => 'contacts#thanks'
+get  'contacts/thanks'                     => 'contacts#thanks'
 resources :contacts
 end

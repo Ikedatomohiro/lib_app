439180d7d16a246a8ca9de960661bc7d925611c8
tomohiro <to.tomo.tomohiro@gmail.com>
2020-06-28 00:13:08 +0900

    いいねのカウントをupdate_columnsにして、更新時間を更新しないようにした

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
     def create
         @impression = Impression.find_by(id: params[:impression_id])
         @impression.like_count += 1
-        @impression.update(like_count: @impression.like_count)
+        @impression.update_columns(like_count: @impression.like_count)
         if signed_in? && params[:user_id].present?
             like = Like.create(impression_id: like_params[:impression_id], user_id: like_params[:user_id])
         end
@@ -16,7 +16,7 @@ class LikesController < ApplicationController
     def destroy
         @impression = Impression.find_by(id: params[:impression_id])
         @impression.like_count -= 1
-        @impression.update(like_count: @impression.like_count)
+        @impression.update_columns(like_count: @impression.like_count)
         if signed_in? && params[:user_id].present?
             like = Like.find_by(impression_id: like_params[:impression_id], user_id: like_params[:user_id])
             like.destroy!
